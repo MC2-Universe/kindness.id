b02c1d708588b617f762e3e57fbb514377199ecc
Joel Serino <thinq4yourself@users.noreply.github.com>
2025-02-11 08:41:02 -0800

    updated icons

--- src/app/explorer/_components/YourGuilds.tsx
+++ src/app/explorer/_components/YourGuilds.tsx
@@ -7,6 +7,7 @@ import { Anchor } from "@/components/ui/Anchor"
 import { Button, buttonVariants } from "@/components/ui/Button"
 import { Card } from "@/components/ui/Card"
 import { useYourGuilds } from "@/hooks/useYourGuilds"
+import { useColorMode } from "@chakra-ui/react"
 import { Plus, SignIn } from "@phosphor-icons/react/dist/ssr"
 import { useSetAtom } from "jotai"
 import Image from "next/image"
@@ -14,8 +15,12 @@ import Image from "next/image"
 export const YourGuilds = () => {
   const { isWeb3Connected } = useWeb3ConnectionManager()
   const { data, isLoading } = useYourGuilds()
+  const { colorMode } = useColorMode()
   const setIsWalletSelectorModalOpen = useSetAtom(walletSelectorModalAtom)
-
+  const icon =
+    colorMode === "light"
+      ? "/landing/kindness_badge_light.svg"
+      : "/landing/kindness_badge_dark.svg"
   if (!isLoading && !data?.length)
     return (
       <Card className="mt-2 mb-8 flex flex-col items-stretch justify-between gap-8 p-6 font-semibold sm:flex-row sm:items-center">
@@ -28,10 +33,10 @@ export const YourGuilds = () => {
           ) : (
             <>
               <Image
-                src="/landing/robot.svg"
+                src={icon}
                 width={32}
                 height={32}
-                alt="Collab Robot icon"
+                alt="Kindness icon"
                 className="size-8 min-w-8"
               />
               <span>Sign in to create and use your Kindness Profile</span>
